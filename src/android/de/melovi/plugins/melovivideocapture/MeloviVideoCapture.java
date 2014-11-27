bfc015c0803a3d5b9ad308ce5ce2bafdf748a635
Doppelherz <doppelherz@live.de>
2014-11-27 10:18:12 +0100

    teste aus

--- src/android/de/melovi/plugins/melovivideocapture/MeloviVideoCapture.java
+++ src/android/de/melovi/plugins/melovivideocapture/MeloviVideoCapture.java
@@ -25,9 +25,13 @@ public class MeloviVideoCapture extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-      
+     
+myWebView.post(new Runnable() {
+    @Override
+    public void run() {
         webView.loadUrl("javascript:alert('hello');");
-
+    }
+});
     
         String outputFile = null;
         final MediaRecorder myRecorder;
