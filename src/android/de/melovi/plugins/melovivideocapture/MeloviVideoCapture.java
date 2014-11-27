2b5f46b7e4c34aad66d8d9e4b2ded3518275928a
Doppelherz <doppelherz@live.de>
2014-11-27 13:42:54 +0100

    teste aus

--- src/android/de/melovi/plugins/melovivideocapture/MeloviVideoCapture.java
+++ src/android/de/melovi/plugins/melovivideocapture/MeloviVideoCapture.java
@@ -1,3 +1,24 @@
+package de.melovi.plugins.melovivideocapture;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import org.json.JSONException;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Environment;
+import org.apache.cordova.Config;
+import org.apache.cordova.DroidGap;
+import android.os.Bundle;
+import org.apache.cordova.CordovaWebView;
+import android.webkit.WebView;
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -8,12 +29,18 @@ import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+
 public class MeloviVideoCapture extends CordovaPlugin {
-public static final String TAG = "Cool Plugin";
-/**
+
+
+    private static final String LOG_TAG = "MeloviVideoCapture";
+    public static final String TAG = "MeloviVideoCapture";
+
+    /**
 * Constructor.
 */
-public MeloviVideoCapture() {}
+public CoolPlugin() {}
 /**
 * Sets the context of the Command. This can then be used to do things like
 * get file paths associated with the Activity.
@@ -21,20 +48,104 @@ public MeloviVideoCapture() {}
 * @param cordova The context of the main Activity.
 * @param webView The CordovaWebView Cordova is running in.
 */
+
 public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 super.initialize(cordova, webView);
 Log.v(TAG,"Init MeloviVideoCapture");
 }
-public boolean execute(final String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-final int duration = Toast.LENGTH_SHORT;
+
+    @Override
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+    
+        Log.i("System.out", "Hier ist eine Nachricht");
+
+        this.WebView.loadUrl("javascript:alert('hello');");
+
+        final int duration = Toast.LENGTH_SHORT;
 // Shows a toast
-Log.v(TAG,"CoolPlugin received:"+ action);
+Log.v(TAG,"MeloviVideoCapture received:"+ action);
 cordova.getActivity().runOnUiThread(new Runnable() {
 public void run() {
 Toast toast = Toast.makeText(cordova.getActivity().getApplicationContext(), action, duration);
 toast.show();
 }
-});
-return true;
+
+        String outputFile = null;
+        final MediaRecorder myRecorder;
+
+        outputFile = Environment.getExternalStorageDirectory().
+                  getAbsolutePath() + "/TestRecording.m4a";
+
+        myRecorder = new MediaRecorder();
+        myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        myRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        myRecorder.setAudioSamplingRate(44100);
+        myRecorder.setAudioChannels(1);
+        myRecorder.setAudioEncodingBitRate(32000);
+        myRecorder.setOutputFile(outputFile);
+
+              myRecorder.start();
+             
+
+        CountDownTimer countDowntimer = new CountDownTimer(7000, 1000) {
+        public void onTick(long millisUntilFinished) {}
+
+        public void onFinish() {
+            myRecorder.stop();
+            myRecorder.release(); 
+                           
+
+        }
+        };
+
+        countDowntimer.start();
+
+        return true;
+
+    }
+
+
+
+    /** "
+   * Sets up an intent to capture video.  Result handled by onActivityResult()
+   */
+  private void recordAudio(String action, JSONArray args, final CallbackContext callbackContext) {
+   
+              String outputFile = null;
+        final MediaRecorder myRecorder;
+
+        outputFile = Environment.getExternalStorageDirectory().
+                  getAbsolutePath() + "/TestRecording.m4a";
+
+        myRecorder = new MediaRecorder();
+        myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        myRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        myRecorder.setAudioSamplingRate(44100);
+        myRecorder.setAudioChannels(1);
+        myRecorder.setAudioEncodingBitRate(32000);
+        myRecorder.setOutputFile(outputFile);
+
+              myRecorder.start();
+ 
+    
+        
+
+        CountDownTimer countDowntimer = new CountDownTimer(7000, 1000) {
+        public void onTick(long millisUntilFinished) {}
+
+        public void onFinish() {
+            myRecorder.stop();
+            myRecorder.release();   
+                     
+
+        }
+        };
+
+        countDowntimer.start();
+          
+            
 }
-}
+
+ }
