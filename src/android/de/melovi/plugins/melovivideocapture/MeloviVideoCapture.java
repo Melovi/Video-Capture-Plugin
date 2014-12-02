d01b19fac0d051590f0d84915a3280433242a852
Doppelherz <doppelherz@live.de>
2014-12-02 14:31:45 +0100

    teste aus

--- src/android/de/melovi/plugins/melovivideocapture/MeloviVideoCapture.java
+++ src/android/de/melovi/plugins/melovivideocapture/MeloviVideoCapture.java
@@ -44,10 +44,7 @@ import java.io.IOException;
 
 public class MeloviVideoCapture extends CordovaPlugin {
 
-    private Camera mCamera;
-    private MediaRecorder mMediaRecorder=null;
-    public static final int MEDIA_TYPE_IMAGE = 1;
-    public static final int MEDIA_TYPE_VIDEO = 2;
+
 
   private CallbackContext callbackContext;        // The callback context from which we were invoked.
   private long limit;                             // the number of pics/vids/clips to take
