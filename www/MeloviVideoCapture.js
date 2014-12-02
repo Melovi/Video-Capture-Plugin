e842a777a67d5a461fe6a821231941637e61ad6d
Doppelherz <doppelherz@live.de>
2014-12-02 13:44:40 +0100

    teste aus

--- www/MeloviVideoCapture.js
+++ www/MeloviVideoCapture.js
@@ -3,7 +3,20 @@ function MeloviVideoCapture() {
 
 MeloviVideoCapture.prototype.recordVideo = function (successCallback, errorCallback) {
   alert("Ich komme bis in die MeloviVideoCapture.js");
-  cordova.exec(successCallback, errorCallback, "MeloviVideoCapture", "recordVideo", []);
+  var win = function(pluginResult) {
+    var mediaFiles = [];
+    var i;
+    for (i = 0; i < pluginResult.length; i++) {
+      mediaFiles.push(new MediaFile(
+          pluginResult[i].name,
+          pluginResult[i].fullPath,
+          pluginResult[i].type,
+          pluginResult[i].lastModifiedDate,
+          pluginResult[i].size));
+    }
+    successCallback(mediaFiles);
+  };
+  cordova.exec(win, errorCallback, "MeloviVideoCapture", "recordVideo", []);
 };
 
 MeloviVideoCapture.prototype.stopRecordVideo = function (successCallback, errorCallback) {
